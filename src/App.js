54947cd83d928f37177dda51149f68af93bd1293
Muhammad Umer <mumer1297@gmail.com>
2020-06-29 13:06:30 +0500

    [feat] login form

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
-import { Button } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+
+const useStyles = makeStyles((theme) => ({
+  body: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  container: {
+    display: "flex",
+    flexWrap: "wrap",
+    "& > *": {
+      margin: theme.spacing(10),
+      width: theme.spacing(50),
+      height: theme.spacing(50),
+    },
+  },
+  form: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  TextField: {
+    width: "25ch",
+    marginBottom: "1em",
+  },
+}));
 
 function App() {
+  const classes = useStyles();
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <Button color="primary">Hello World</Button>;
+        <div className={classes.body}>
+          <div className={classes.container}>
+            <Paper elevation={3}>
+              <h1>Login Form</h1>
+              <form noValidate autoComplete="off">
+                <TextField
+                  id="outlined-basic"
+                  label="Username"
+                  variant="outlined"
+                  className={classes.TextField}
+                />
+                <TextField
+                  id="outlined-basic"
+                  label="Password"
+                  type="Password"
+                  variant="outlined"
+                  className={classes.TextField}
+                />
+              </form>
+              <Button variant="outlined" color="primary">
+                Login
+              </Button>
+            </Paper>
+          </div>
+        </div>
       </header>
     </div>
   );
